31ec1662665f0ccdb34cdc3f1807221d8bf1789f
Vladyslav Kucherenko <vlados.soc@gmail.com>
2024-12-12 16:09:49 +0200

    Solution

--- main.py
+++ main.py
@@ -1,8 +1,47 @@
-def main() -> None:
-    # this is an entrypoint, DO NOT WRITE ALL CODE HERE !!!
-    # ToDo remove these comments
+import csv
+import datetime
+from os.path import exists
 
-    ...
+import requests
+import os
+
+
+def get_joke():
+    try:
+        response = requests.get('https://official-joke-api.appspot.com/random_joke')
+        setup = response.json().get("setup")
+        punchline = response.json().get("punchline")
+        return setup, punchline
+    except requests.RequestException:
+        print("Error")
+
+
+def save_joke_to_scv(time_joke: datetime, setup: str, punchline: str):
+    file_name = "jokes_history.csv"
+    file_exists = os.path.isfile(file_name)
+
+    with open(file_name, mode="a", newline="", encoding="utf-8") as file:
+        writer = csv.writer(file)
+        if not file_exists:
+            writer.writerow(["Timestamp", "Setup", "Punchline"])
+        writer.writerow([time_joke, setup, punchline])
+
+
+def print_joke():
+    setup, punchline = get_joke()
+    print(f"Here's your joke:\n{setup}")
+    input("Press Enter to see the punchline...")
+    print(f"{punchline}\n")
+    time_joke = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    save_joke_to_scv(time_joke, setup, punchline)
+
+
+def main() -> None:
+    while True:
+        print_joke()
+        user_answer = input("Would you like another joke? (yes/no): ")
+        if user_answer.lower() == "no":
+            break
 
 
 if __name__ == "__main__":
