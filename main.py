1d10f6af78659dbf7a04bceda0b835fcccbde1d6
roman <skyfoxwork@gmail.com>
2024-12-12 18:03:13 +0200

    second

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import requests
-import json
+import csv
 
 
 def get_request() -> list:
@@ -13,18 +13,29 @@ def main() -> None:
     while True:
         answer = get_request()
 
+        data = [
+            ["Timestamp", "Setup", "Punchline"],
+            ["", answer[0], answer[1]],
+        ]
+
+        with open("jokes_history.csv", mode='w', newline='') as file:
+            csv_writer = csv.writer(file)
+            csv_writer.writerows(data)
+
         print("Here's your joke:")
         print(answer[0])
 
-        print("\nPress Enter to see the punchline...")
-
-        user_answer = input("\nWould you like another joke? (yes/no):")
-
-        if user_answer == "":
-            print(answer[1])
+        input("\nPress Enter to see the punchline...")
+        print(answer[1])
+        user_answer = input("\nWould you like another joke? (yes/no): ")
 
         if user_answer == "no":
             break
+
+
+
+
+
 
 
 if __name__ == "__main__":
