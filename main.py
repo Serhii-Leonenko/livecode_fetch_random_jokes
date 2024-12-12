a3773cc317adba21f727c22b8ffdab811d982a0a
Serhii <serhijkrasa64uk5@gmail.com>
2024-12-12 18:18:02 +0200

    solution

--- main.py
+++ main.py
@@ -1,8 +1,22 @@
-def main() -> None:
-    # this is an entrypoint, DO NOT WRITE ALL CODE HERE !!!
-    # ToDo remove these comments
+import csv
 
-    ...
+from random_jokes import random_joke
+
+
+def main() -> None:
+    file_name = "joke_history.csv"
+    try:
+        with open(file_name, "w", newline="") as file:
+            writer = csv.writer(file)
+            field = ["Timestamp", "Setup", "Punchline"]
+            row_list = random_joke()
+            writer.writerow(field)
+
+            writer.writerows(row_list)
+    except IOError:
+        print ("Could not read file:", file_name)
+
+
 
 
 if __name__ == "__main__":
